214fc29ae609f574f7f1d327832fc40c759514d7
awinooliyo <erick.otieno89@gmail.com>
2024-07-30 18:22:00 +0300

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ Hello World"""
-    return render_template('2-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
