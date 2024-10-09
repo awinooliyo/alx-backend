583fac2acddb8156793fd9f9c0aef13d53014d6f
awinooliyo <erick.otieno89@gmail.com>
2024-10-09 07:57:21 +0300

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -6,7 +6,7 @@ const queue = kue.createQueue();
 // Job data object
 const jobData = {
   phoneNumber: '1234567890',
-  message: 'This is a test notification message',
+  message: 'This is the code to verify your account',
 };
 
 // Create a job in the push_notification_code queue
